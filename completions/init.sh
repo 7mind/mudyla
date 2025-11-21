a511bb45672bf4797e432ecc6edd530663ee7bf2
Pavel Shirshov <pshirshov@eml.cc>
2025-11-21 19:57:40 +0000

    wip

--- completions/init.sh
+++ completions/init.sh
@@ -1,6 +1,7 @@
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-${(%):-%N}}")" >/dev/null 2>&1 && pwd)"
 
 COMPLETION_DIR="${SCRIPT_DIR}/../share/mudyla"
+COMPLETION_DIR="${COMPLETION_DIR:a}"
 
 if [ -n "$ZSH_VERSION" ]; then
   . "${COMPLETION_DIR}/init.zsh"
