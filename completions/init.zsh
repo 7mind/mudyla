1d41d3fbe94c59ec7b8d899804e08738c4b6356f
Pavel Shirshov <pshirshov@eml.cc>
2025-11-21 19:50:49 +0000

    wip

--- completions/init.zsh
+++ completions/init.zsh
@@ -1,8 +1,8 @@
 # Mudyla zsh completion initializer
 # Source this to add Mudyla completions to your shell.
 
-FPATH="${0:A:h}":$FPATH
-#fpath=("${0:A:h}" $fpath)
+cd "${0:A:h}"
+fpath=("${0:A:h}" $fpath)
 autoload -Uz compinit
 compinit -i >/dev/null 2>&1 || true
 autoload -Uz _mdl 2>/dev/null || true
